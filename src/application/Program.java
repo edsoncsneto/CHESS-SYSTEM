7368af786e0894b38220c15033b8cd5bf41ac713
Edson Costa de Siqueira Neto <edsonsiqueiraneto@gmail.com>
2024-01-04 19:43:10 -0300

    Avanços no projeto. Última alteração: colocando peças no tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("oi");
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }

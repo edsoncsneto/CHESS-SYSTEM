caaa2663a0c534d25951d34736185440991a96c8
Edson Costa de Siqueira Neto <edsonsiqueiraneto@gmail.com>
2024-01-10 22:14:36 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheck()) {
 			
 			try {
 				UI.clearScreen();
@@ -50,7 +50,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
-		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 		
 	}
 

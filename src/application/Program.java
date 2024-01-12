e1f9b3fd3e87be27994033a33c950c0568a56d60
Edson Costa de Siqueira Neto <edsonsiqueiraneto@gmail.com>
2024-01-11 22:46:39 -0300

    Queen

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(!chessMatch.getCheck()) {
+		while(!chessMatch.getCheckMate()) {
 			
 			try {
 				UI.clearScreen();
